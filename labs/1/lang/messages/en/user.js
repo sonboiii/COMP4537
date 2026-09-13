c5d21753fcb3b257a0617e44334180a115f247f2
sonboiii <hoangson27203@gmail.com>
2026-09-13 15:50:20 -0700

    Initialize COMP4537 with Lab 1

---
+++ labs/1/lang/messages/en/user.js
@@ -0,0 +1,7 @@
+export const STRINGS = {
+    LABEL_NUM_BUTTONS: "How many buttons to create? ",
+    BTN_GO: "Go!",
+    ERR_INVALID_RANGE: "Please enter a number between 3 and 7.",
+    MSG_EXCELLENT: "Excellent memory!",
+    MSG_WRONG: "Wrong order!"
+};
